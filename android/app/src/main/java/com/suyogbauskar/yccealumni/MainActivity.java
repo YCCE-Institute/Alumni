ec42c775d62b026a926a83e1d41135386e1fa199
Suyog-Bauskar <suyogbauskar2004@gmail.com>
2025-05-03 21:31:26 +0530

    Test 9

--- android/app/src/main/java/com/suyogbauskar/yccealumni/MainActivity.java
+++ android/app/src/main/java/com/suyogbauskar/yccealumni/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         city.put("country", "USA");
         city.put("name", "Prod release");
         city.put("state", "CA");
-        city.put("test", "test 8");
+        city.put("test", "test 9");
 
         FirebaseFirestore.getInstance().collection("cities").document("LA")
                 .set(city)
